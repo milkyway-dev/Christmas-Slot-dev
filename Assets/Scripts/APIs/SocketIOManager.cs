d732826c39228a7edf999157944278fa9e7a9b71
Sayan <sayanguha.underpinservices@gmail.com>
2024-08-24 14:52:37 +0530

    latest changes

--- Assets/Scripts/APIs/SocketIOManager.cs
+++ Assets/Scripts/APIs/SocketIOManager.cs
@@ -37,8 +37,8 @@ public class SocketIOManager : MonoBehaviour
     [SerializeField]
     internal JSHandler _jsManager;
 
-    // protected string TestSocketURI = "https://dev.casinoparadize.com";
-    protected string TestSocketURI = "http://localhost:5000";
+    protected string TestSocketURI = "https://dev.casinoparadize.com";
+    // protected string TestSocketURI = "http://localhost:5000";
     protected string SocketURI = null;
     //protected string SocketURI = "https://7p68wzhv-5000.inc1.devtunnels.ms/";
 
@@ -49,6 +49,9 @@ public class SocketIOManager : MonoBehaviour
     private const int maxReconnectionAttempts = 6;
     private readonly TimeSpan reconnectionDelay = TimeSpan.FromSeconds(10);
 
+    internal bool isLoaded = false;
+
+    internal bool SetInit=false;
     private void Start()
     {
         //Debug.unityLogger.logEnabled = false;
@@ -67,7 +70,6 @@ public class SocketIOManager : MonoBehaviour
 
     string myAuth = null;
 
-    internal bool isLoaded = false;
 
     private void Awake()
     {
@@ -401,16 +403,23 @@ public class SocketIOManager : MonoBehaviour
         {
             case "InitData":
                 {
-                    Debug.Log(jsonObject);
                     initialData = myData.message.GameData;
                     initUIData = myData.message.UIData;
                     playerdata = myData.message.PlayerData;
                     bonusdata = myData.message.BonusData;
-                    List<string> InitialReels = ConvertListOfListsToStrings(initialData.Reel);
-                    List<string> LinesString = ConvertListListIntToListString(initialData.Lines);
-                    InitialReels = RemoveQuotes(InitialReels);
-                    GambleLimit = myData.message.maxGambleBet;
-                    PopulateSlotSocket(InitialReels, LinesString);
+                    if (!SetInit)
+                    {
+                        Debug.Log(jsonObject);
+                        List<string> LinesString = ConvertListListIntToListString(initialData.Lines);
+                        List<string> InitialReels = ConvertListOfListsToStrings(initialData.Reel);
+                        InitialReels = RemoveQuotes(InitialReels);
+                        PopulateSlotSocket(InitialReels, LinesString);
+                        SetInit = true;
+                    }
+                    else
+                    {
+                        RefreshUI();
+                    }
                     break;
                 }
             case "ResultData":
@@ -442,6 +451,10 @@ public class SocketIOManager : MonoBehaviour
         }
     }
 
+    private void RefreshUI()
+    {
+        uiManager.InitialiseUIData(initUIData.AbtLogo.link, initUIData.AbtLogo.logoSprite, initUIData.ToULink, initUIData.PopLink, initUIData.paylines);
+    }
     private void PopulateSlotSocket(List<string> slotPop, List<string> LineIds)
     {
         //for (int i = 0; i < slotPop.Count; i++)
